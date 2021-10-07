1e6f4170a763bf6d6c97832ef4e1b5e35dbff39d
han0gu <imgum9@gmail.com>
2021-10-07 11:40:34 +0900

    Modify: sign-up layout complete

---
+++ src/pages/SignUp/components/PersonalInfoPolicy2.js
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+import './PersonalInfoPolicy2.scss';
+
+export default class PersonalInfoPolicy2 extends Component {
+  render() {
+    return (
+      <div className='PersonalInfoPolicy2'>
+        <div className='policyContainer'>
+          <p className='mainTitle'>
+            개인정보 수집·이용 동의 <span>&nbsp;(선택)</span>
+          </p>
+          <button onClick={this.props.clickConfirmBtn}>확인</button>
+        </div>
+      </div>
+    );
+  }
+}
