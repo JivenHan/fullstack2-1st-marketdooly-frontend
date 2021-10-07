3e9964e868e79891d1253a0633d63b32fbcefed8
han0gu <imgum9@gmail.com>
2021-10-07 11:18:01 +0900

    Modify: refactoring

--- src/pages/FindPw/FindPw.js
+++ src/pages/FindPw/FindPw.js
@@ -32,30 +32,30 @@ export default class FindPw extends Component {
         <div className='findPwContainer'>
           <h3>비밀번호 찾기</h3>
           <form id='userInfoForm' action='./findPw' method='POST'>
-            <strong>이름</strong>
+            <label>이름</label>
             <input
               type='text'
               name='userName'
               placeholder='고객님의 이름을 입력해주세요'
               onChange={handleInput}
               required
-            ></input>
-            <strong>아이디</strong>
+            />
+            <label>아이디</label>
             <input
               type='text'
               name='userId'
               placeholder='가입 시 등록하신 아이디를 입력해주세요'
               onChange={handleInput}
               required
-            ></input>
-            <strong>이메일</strong>
+            />
+            <label>이메일</label>
             <input
               type='email'
               name='userEmail'
               placeholder='가입 시 등록하신 이메일 주소를 입력해주세요'
               onChange={handleInput}
               required
-            ></input>
+            />
           </form>
           <button
             className={isValidInput ? 'btnFind valid' : 'btnFind invalid'}
